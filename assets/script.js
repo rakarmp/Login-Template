24575f37c8aaf04cf9d7ef2813c546dd2b1c0cfb
grizz12 <raka_088@smkassalaambandung.sch.id>
2023-01-10 09:05:20 +0700

    validation

---
+++ assets/script.js
@@ -0,0 +1,36 @@
+// Check if passwords match
+$("#pwdId, #confirmPassword").on("keyup", function () {
+  if (
+    $("#newPassword").val() != "" &&
+    $("#confirmPassword").val() != "" &&
+    $("#confirmPassword").val() == $("#confirmPassword").val()
+  ) {
+  }
+});
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+  "use strict";
+  window.addEventListener(
+    "load",
+    function () {
+      // Fetch all the forms we want to apply custom Bootstrap validation styles to
+      var forms = document.getElementsByClassName("needs-validation");
+      // Loop over them and prevent submission
+      var validation = Array.prototype.filter.call(forms, function (form) {
+        form.addEventListener(
+          "submit",
+          function (event) {
+            if (form.checkValidity() === false) {
+              event.preventDefault();
+              event.stopPropagation();
+            }
+            form.classList.add("was-validated");
+          },
+          false
+        );
+      });
+    },
+    false
+  );
+})();
